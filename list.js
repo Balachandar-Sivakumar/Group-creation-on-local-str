0ab23820c8f9e07f88a95a456fe546444878a96c
Balachandar-Sivkumar <balachandarsdckap@gmail.com>
2025-03-10 11:31:10 +0530

    corrected some errors and valuation

---
+++ list.js
@@ -0,0 +1,55 @@
+// let local = JSON.parse(localStorage.getItem('data')) || []; 
+
+// addEventListener('DOMContentLoaded',()=>{
+
+//     local.forEach(n=>{
+//           tablelist(n)
+//      })    
+//  });
+ 
+//  function tablelist(n){
+ 
+//      let tbody = document.querySelector('tbody');
+   
+//      let tr = document.createElement('tr'),
+//      admin = document.createElement('td'),
+//      email = document.createElement('td'),
+//      members = document.createElement('td'),
+//      pass = document.createElement('td'),
+//      phone = document.createElement('td'),
+//      dob = document.createElement('td'),
+//      addr = document.createElement('td'),
+//      del = document.createElement('td'),
+//      remove = document.createElement('button'); remove.textContent='Delete';
+ 
+//      admin.append(n.names)
+//      email.append(n.email)
+//      members.append(n.email_mem == "" ? 'No invites' : n.email_mem.join(', '))
+//      pass.append(n.password)
+//      phone.append(n.phone)
+//      dob.append(n.date)
+//      addr.append(n.address)
+//      del.append(remove)
+ 
+//      del.addEventListener('click',()=>{
+//          tr.remove()
+         
+//          local=local.filter(x=> x.names!==n.names)
+ 
+//          localStorage.setItem('data', JSON.stringify(local))
+//      })
+     
+    
+//      tr.append(admin,email,members,pass,phone,dob,addr,del)
+    
+//      tbody.append(tr)
+   
+ 
+//  };
+ 
+
+let ptrn = /^[6-9][0-9]{9}$/
+
+let num = 9585981417
+
+console.log(ptrn.test(num));
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let add_btn = document.getElementById('add-email'),
 let local = JSON.parse(localStorage.getItem('data')) || []; 
 let adderr = document.querySelectorAll('span');    
 
-if (add_btn) { 
+
     add_btn.addEventListener('click', () => {
 
         if (add_inp.value === '' || !add_inp.value.includes('@gmail.com')) {
@@ -17,7 +17,7 @@ if (add_btn) {
         adderr.forEach(n=> n.style.display='none')
         start(add_inp.value);
     });
-}
+
 
 function start(n){
    
@@ -56,22 +56,25 @@ let full_name = document.querySelector('#full-name'),
     submit = document.querySelector('#submitt'),
     arr = []
 
-if(submit){
+
     submit.addEventListener('click',(event)=>{
 
         event.preventDefault();
 
         let error = document.querySelectorAll('small');
 
-        if(full_name.value=='') return error[0].style.display='block';
-        if(email.value=="" || !email.value.includes('@gmail.com')) return error[1].style.display='block';
-        if(pass.value=='' || pass.value !== cnpass.value) return error[2].style.display='block';
-        if(gender == '') return error[3].style.display='block';
-        if(phone.value=='') return error[4].style.display='block';
-        if(gender.value=='')return error[5].style.display='block';
-        if(address.value=='') return error[6].style.display='block' ;
+        if(full_name.value.trim()=='') return error[0].classList.add('error'),error[0].scrollIntoView({block:"center"});
+        if(email.value.trim()=="" || !email.value.includes('@gmail.com')) return error[1].classList.add('error'),error[1].scrollIntoView({block:"center"});
+        if(pass.value.trim()=='' || pass.value !== cnpass.value) return error[2].classList.add('error'),error[2].scrollIntoView({block:"center"});
+        if(pass.value.length<8) return error[3].classList.add('error'),error[3].scrollIntoView({block:"center"});
+        if(gender == '') return error[4].classList.add('error'),error[4].scrollIntoView({block:"center"});
+        let patrn = /^[6-9][0-9]{9}$/;
+        let check = patrn.test(phone.value.trim());
+        if (!check) return error[5].classList.add('error'),error[5].scrollIntoView({block:"center"}); 
+        if(gender.value=='')return error[6].classList.add('error'),error[6].scrollIntoView({block:"center"});
+        if(address.value.trim()=='') return error[7].classList.add('error'),error[7].scrollIntoView({block:"center"}) ;
 
-        error.forEach(n=> n.style.display='none');
+        error.forEach(n=> n.classList.remove('error'));
 
         console.log(address.value);
         
@@ -98,57 +101,11 @@ if(submit){
 
         ul.innerHTML = ''
 
-        if(adderr){
+        
             adderr.forEach(n=> n.style.display='none')
-        }
+        
         
     })
-}
 
-addEventListener('DOMContentLoaded',()=>{
 
-   local.forEach(n=>{
-         tablelist(n)
-    })    
-});
-
-function tablelist(n){
-
-    let tbody = document.querySelector('tbody');
-  if(tbody){
-    let tr = document.createElement('tr'),
-    admin = document.createElement('td'),
-    email = document.createElement('td'),
-    members = document.createElement('td'),
-    pass = document.createElement('td'),
-    phone = document.createElement('td'),
-    dob = document.createElement('td'),
-    addr = document.createElement('td'),
-    del = document.createElement('td'),
-    remove = document.createElement('button'); remove.textContent='Delete';
-
-    admin.append(n.names)
-    email.append(n.email)
-    members.append(n.email_mem == "" ? 'No invites' : n.email_mem.join(', '))
-    pass.append(n.password)
-    phone.append(n.phone)
-    dob.append(n.date)
-    addr.append(n.address)
-    del.append(remove)
-
-    del.addEventListener('click',()=>{
-        tr.remove()
-        
-        local=local.filter(x=> x.names!==n.names)
-
-        localStorage.setItem('data', JSON.stringify(local))
-    })
-    
-   
-    tr.append(admin,email,members,pass,phone,dob,addr,del)
-   
-    tbody.append(tr)
-  }
-
-};
 
